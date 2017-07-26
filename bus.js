15d714b210d890b3c0c06109a5f3c1ce9d32f4a3
anaYesi <anayesenia026@gmail.com>
2017-07-26 11:53:26 -0500

    agrege html y js

--- bus.js
+++ bus.js
@@ -1,12 +1,33 @@
 var N=32;
 var asientos = new Array(N);
 var asientoSelec = -1;
- for (var i = 0; i < N; i++) {
-      asientos[i] = undefined;
-   }
+ 
 
 function buscar(dni)
 {
+	var d1 = document.getElementById("dniInput").value;
+	var datos_mostrar = "";
+	for (var i = 0; i < N; i++) 
+	{
+       if(asientos[i] != undefined)
+       {
+       	var as = asientos[i];
+      	 if(d1 == as.dni)
+      	 {
+      	 	var a=asientos[i];
+      	var s = "Nombre: "+  a.nombre +"\n"+ "Apellidos: "+ a.apellido+
+      	"\n"+"DNI:"+a.dni +"\n"+ "asiento: "+ (i+1)+"\n";
+      	datos_mostrar += s;
+			alert("hola");	
+	     }
+	     else
+	     	alert("no hola")
+
+       }
+		
+	}
+   document.getElementById("listar2").innerText = datos_mostrar;
+
 	
 }
 
@@ -46,21 +67,22 @@ function cancelar()
 }
 function listar()
 {
-	var html = "";
+	var datos_mostrar = "";
 	for (var i = 0; i < N; i++) 
 	{
       if(asientos[i] != undefined)
       {
-      	name.innerHTML= asientos[i]
-      	var nombresitos = asientos[i];
-      	var apellidos = asientos[i];
-      	var dni=asientos[i];
-      	var s = "Nombre: "+  nombresitos.nombre + "apellidos"+ apellidos.apellido+"dni"+dni.dni;
-      	html += s;
+      	//name.innerHTML= asientos[i]
+      //	var nombresitos = asientos[i];
+      	//var apelliditos = asientos[i];
+      	var a=asientos[i];
+      	var s = "Nombre: "+  a.nombre +"\n"+ "Apellidos: "+ a.apellido+
+      	"\n"+"DNI:"+a.dni +"\n"+ "asiento: "+ (i+1)+"\n";
+      	datos_mostrar += s;
       }
     }
-    console.log(html);
-   document.getElementById("listar1").innerText = html;
+    //console.log(datos_mostrar);
+   document.getElementById("listar1").innerText = datos_mostrar;
 
 }
 
@@ -73,8 +95,13 @@ function redirect(event){
 
     asientoSelec=(event.target.textContent);
     var mostrar=document.getElementById("mostrar");
-    mostrar.innerHTML=asientoSelec;
+    mostrar.innerHTML="asiento seleccionado: "+asientoSelec ;
     celda = event.target;
+
 }
 
+function clear()
+{
+
+}
 
